43a9e418ec703ca09fe3f8f6ab6af07b1e55d3e6
Virginia Atenza <virginia.atenza@gmail.com>
2020-04-01 18:07:35 +0200

    Navbar and banner

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,5 +10,7 @@ import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
