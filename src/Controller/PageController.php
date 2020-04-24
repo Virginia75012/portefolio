0cb062a666c77c3b304726795b09475ac90ea35b
Virginia Atenza <virginia.atenza@gmail.com>
2020-04-24 14:28:27 +0200

    Add contact form

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Notification\ContactNotification;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class PageController extends AbstractController
 {
 	/**
 	 * @Route("/", name="home")
 	 */
-	public function index()
+	public function index(Request $request, ContactNotification $notification): Response
 	{
+		$contact = new Contact();
+		$form = $this->createForm(ContactType::class, $contact);
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$notification->notify($contact);
+			$this->addFlash('success', 'Votre email a bien été envoyé');
+			return $this->redirectToRoute('home');
+		}
+
+
 		return $this->render('page/index.html.twig', [
 			'controller_name' => 'PageController',
+			'form' => $form->createView()
 		]);
 	}
 }
